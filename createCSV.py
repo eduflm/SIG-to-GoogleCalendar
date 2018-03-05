20d206b87d0f334bd5552c45657721d0bf30f772
Eduardo Lima <eduardo.fernandodelima@gmail.com>
2018-03-05 19:57:14 -0300

    Fetching data and converting

--- createCSV.py
+++ createCSV.py
@@ -1,6 +1,5 @@
 import csv
-
-#For now, this file only exists as a base for the future
+import datetime
 
 class CSV:
 	def __init__(self,schedule, startDate, endDate, fileName):
@@ -9,15 +8,36 @@ class CSV:
 		self.endDate = endDate
 		self.fileName = fileName
 
+	def correctNumber(self,number):
+		number = list(number)
+		if(number[0] == 0):
+			del number[0]
+		return ''.join(number)
+
+
+	def createDate(self, date):
+		splitDate = date.split('/')
+		splitDate[0] = self.correctNumber(splitDate[0])
+		splitDate[1] = self.correctNumber(splitDate[1])
+		return datetime.date(int(splitDate[2]), int(splitDate[1]), int(splitDate[0]))
+
 
 
 	def writeFile(self):
+		print('Writing in '+self.fileName)
 		with open(self.fileName, 'w', newline='') as csvfile:
-
 			fieldnames = ['Subject', 'Start Date', 'Start Time', 'End Date', 'End Time', 'Location', 'Private']
 			writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
 			writer.writeheader()
-			writer.writerow({'Subject' : 'GCC-199 - Computação Gráfica', 'Start Date' : '02/27/2018', 'Start Time' : '10:00 AM', 'End Date' : '02/27/2018', 'End Time' : '11:40 AM', 'Location':'PV2-106', 'Private' : 'True'})
-			writer.writerow({'Subject' : 'GCC-199 - Computação Gráfica', 'Start Date' : '02/28/2018', 'Start Time' : '10:00 AM', 'End Date' : '02/28/2018', 'End Time' : '11:40 AM', 'Location':'PV2-106', 'Private' : '1True'})
-			writer.writerow({'Subject' : 'GCC-199 - Computação Gráfica', 'Start Date' : '02/29/2018', 'Start Time' : '10:00 AM', 'End Date' : '02/29/2018', 'End Time' : '11:40 AM', 'Location':'PV2-106', 'Private' : 'True'})
+			self.startDate = self.createDate(self.startDate)
+			self.endDate = self.createDate(self.endDate)
+			actualDate = self.startDate
+			for i in range (abs(self.startDate-self.endDate).days + 1):
+				weekDay = actualDate.weekday()
+				for classs in self.schedule[weekDay]:
+					classs[1] = actualDate.strftime('%m/%d/%Y')
+					classs[3] = actualDate.strftime('%m/%d/%Y')
+					writer.writerow({'Subject' : classs[0], 'Start Date' : classs[1], 'Start Time' : classs[2], 'End Date' : classs[3], 'End Time' : classs[4], 'Location': classs[5], 'Private' : 'True'})
+				actualDate = actualDate + datetime.timedelta(days=1)	
+		print('Done!')
+
--- sigToAgenda.py
+++ sigToAgenda.py
@@ -2,6 +2,8 @@ import requests
 import sys
 import getpass
 import urllib3
+import numpy as np
+from pathlib import Path
 from bs4 import BeautifulSoup
 from createCSV import CSV
 from urllib.parse import urljoin
@@ -16,16 +18,39 @@ class Schedule:
 	def getClassSchedule(self):
 		pageData = self.connectToSIG()
 		self.findFormAndLogin(pageData)
+		print ("Extracting data...")
 		schedulePage = self.session.get(self.scheduleURL, verify=False)
 		soup = BeautifulSoup(schedulePage.text, 'html.parser')
 		scheduleBoard = soup.find('tbody')
 		matrix = [[td.text for td in tr.findAll('td')] for tr in scheduleBoard.findAll('tr')]
+		shceduleDict = {0: [], 1: [], 2: [], 3: [], 4: [], 5: [], 6: []}
 		for index, row in enumerate(matrix):
 			matrix[index][0] = self.correctTime(row[0])
+		matrix = np.array(matrix)
 		for row in matrix:
-			print (row)			
+			for i in range (1, 8):
+				if row[i] != '-':
+					classAndLocation = row[i].split(' - ')
+					classCode = classAndLocation[0].split(' ')[0]
+					className = classCode+' - '+soup.find('abbr', string=classCode)['title'].split(' / ')[0]
+					scheduleIndex = i-2
+					if (scheduleIndex < 0):
+						scheduleIndex = 6
+					shceduleDict[i-2].append([className,'',row[0], '', self.endDate(row[0]), classAndLocation[1]])
+		print("Data extracted!")
+		return shceduleDict
 
-		#Como estamos em período de recesso, ainda não é possível testar o programa
+	def endDate(self,time):
+		hour = int(time.split(':')[0])
+		period = time.split(':')[1]
+
+		hour += 1
+		if (hour == 12):
+			return (str(hour)+':00 PM')
+		elif (period == "00 PM"):
+			return (str(hour)+':00 PM')
+		else:
+			return (str(hour)+':00 AM')
 
 	def correctTime(self, time):
 		hour = int(time.split(':')[0])
@@ -76,19 +101,32 @@ def getInfo():
 	info = {}
 	info['login'] = input("Enter your SIG login: ")
 	info['password'] = getpass.getpass(prompt='Enter your SIG password: ')
-	#info['startDate'] = input("Enter the start date (MM/DD/YYYY): ")
-	#info['endDate'] = input("Enter the end date (MM/DD/YYYY): ")
-	info['startDate'] = '01/03/2018'
-	info['endDate'] = '09/03/2018'
+	info['startDate'] = input("Enter the start date (DD/MM/YYYY): ")
+	info['endDate'] = input("Enter the end date (DD/MM/YYYY): ")
 	info['fileName'] = 'example.csv'
+	return info
+
+def getInfoFromFile(fileData):
+	data = fileData.read().splitlines()
+	info = {}
+	info['login'] = data[0]
+	info['password'] = data[1]
+	info['startDate'] = data[2]
+	info['endDate'] = data[3]
+	info['fileName'] = data[4]
 	return info
 
 
 if __name__ == "__main__":
 	urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-	info = getInfo()
+	configFile = Path("data.config")
+	if(configFile.exists()):
+		configFile = open('data.config', "r")
+		info = getInfoFromFile(configFile)
+	else:
+		info = getInfo()
 	schedule = Schedule(info)
-	schedule.getClassSchedule()
+	schedule = schedule.getClassSchedule()
 	csvFile = CSV(schedule, info['startDate'], info['endDate'], info['fileName'])
 	csvFile.writeFile()
 
